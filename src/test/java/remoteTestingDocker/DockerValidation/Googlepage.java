20e0b882c997ea461a74086e47c848b7b0034089
Saidinesh Narisetti <saidinesh.narisetti@thoughtworks.com>
2023-06-19 16:23:48 +0530

    removing edge browser

--- src/test/java/remoteTestingDocker/DockerValidation/Googlepage.java
+++ src/test/java/remoteTestingDocker/DockerValidation/Googlepage.java
@@ -7,16 +7,16 @@ import org.testng.annotations.Test;
 
 public class Googlepage extends LaunchBrowser{
 	@Test(priority=1)
-	public void verifygooglepage(){
+	public void verifygooglepage() throws InterruptedException{
 		driver.get("https://www.google.com/");
 		//String act_title = driver.get("https://www.google.com/");
 		Assert.assertEquals(driver.getTitle(), "Google");
 		System.out.println(driver.getTitle());
+		Thread.sleep(5000);
 	}
 	@Test(priority=2)
 	public void verifyfacebookpage() throws InterruptedException{
-		
-		Thread.sleep(1000);
+		Thread.sleep(5000);
 		driver.get("https://www.facebook.com/");
 		Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
 		System.out.println(driver.getTitle());
